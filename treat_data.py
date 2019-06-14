7db9047c8ff2da5ed00f5377b6d879dc00f01ac5
moisesoliv <moises.oliveira@ecomp.ufsm.br>
2019-06-14 11:44:23 -0300

    treatdata

--- treat_data.py
+++ treat_data.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
-
+import sys
+import os
+import glob
 def calc_mean_and_variance(df):
     scaler = preprocessing.MinMaxScaler(feature_range=(-127, 127))
     #print('loc', df.iloc[:, 2:10])
@@ -27,20 +29,32 @@ def signal_change(df):
         signal_changed = []
         changed_count = 0
         column = df.loc[:, 'Sensor '+str(i)]
-        for j in range(1, 700-1):
+        for j in range(1, 500-1):
             val1 = np.abs(column[j]) - np.abs(column[j-1])
             val2 = np.abs(column[j+1]) - np.abs(column[j])
             #print(column[j-1], column[j], column[j+1], np.sign(val1), np.sign(val2), np.sign(val1)+np.sign(val2))
             if np.sign(val1) + np.sign(val2) == 0 and (val1 != 0 or val2 != 0):
                 signal_changed.append(j)
                 changed_count += 1
-        print("Mudanças de sinal/Contador do Sensor {}: {} / {}".format(i, signal_changed, changed_count))
+        #print("Mudanças de sinal/Contador do Sensor {}: {} / {}".format(i, signal_changed, changed_count))
     return signal_changed
 
-dados = pd.read_csv('./datasets/myo-movimento-david-2019-06-06 11:39:44-.csv')
-print(dados.head(10))
-dados_novo = calc_mean_and_variance(dados)
-zero = zero_crossings(dados_novo)
-signal_change(dados_novo)
-#for i in range(8):
-#    print(zero[i])
+if len(sys.argv) >= 2 :
+    pasta = os.path.dirname(os.path.abspath(__file__)) +"/"+ sys.argv[1]
+    print("Pasta ", pasta)
+    files = glob.glob(pasta+"/*.csv")
+    #print(files)
+    data = []
+    for f in files:
+        #print(f)
+        dados = pd.read_csv(f)
+        #print(dados.head(10))
+        dados_novo = calc_mean_and_variance(dados)
+        zero = zero_crossings(dados_novo)
+        signal_change(dados_novo)
+        #for i in range(8):
+        #print(zero[i])
+        data.append([zero,signal_change])
+    print(data)
+else:
+    print("missing argument")
