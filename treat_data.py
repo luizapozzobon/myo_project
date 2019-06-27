da94313c7fcd7453eda6a62568dd2cd8e4de2560
Luiza <luiza.pozzobon@gmail.com>
2019-06-27 18:04:38 -0300

    treat data zero crossings etc etc

--- treat_data.py
+++ treat_data.py
@@ -4,7 +4,8 @@ from sklearn import preprocessing
 import sys
 import os
 import glob
-
+import scipy as sp
+from scipy import signal
 
 def calc_mean_and_variance(df):
     # Cálculo para as linhas
@@ -21,29 +22,40 @@ def calc_mean_and_variance(df):
     df2['Orientation Variance']   = df[['Orientation 0','Gyro 1','Gyro 2',]].abs().var(axis=1)
     return df.join(df2)
 
-def filter_data(data, low_band=5, mid_band=58, high_band=72, sfreq=200):
+def filter_data(emg, low_band=5, mid_band=58, high_band=72, sfreq=200):
     # TODO testar para dataset inteiro -> APENAS SENSORES
     """
-    time: Time data
     emg: EMG data
     high: high-pass cut off frequency
     low: low-pass cut off frequency
     sfreq: sampling frequency
     """
-    # create highpass filter for EMG for first 5hz
-    low_band = low_band/(sfreq)
-    b1, a1 = sp.signal.butter(4, low_band, btype='highpass')
-    # process EMG signal: filter EMG
-    emg_filtered = sp.signal.filtfilt(b1, a1, emg)
+    for column in emg:
+        # create highpass filter for EMG for first 5hz
+        low_band = low_band/(sfreq)
+        b1, a1 = sp.signal.butter(4, low_band, btype='highpass')
+        # process EMG signal: filter EMG
+        print(emg[column].values)
+        emg_filtered = sp.signal.filtfilt(b1, a1, emg[column].values)
+        print(emg[column].values)
 
-    # normalise cut-off frequencies to sampling frequency
-    high_band = high_band/(sfreq)
-    mid_band = mid_band/(sfreq)
-    # create second bandpass filter for EMG
-    b1, a1 = sp.signal.butter(4, [mid_band, high_band], btype='bandstop')
-    emg_filtered2 = sp.signal.filtfilt(b1, a1, emg_filtered)
+        # normalise cut-off frequencies to sampling frequency
+        high_band = high_band/(sfreq)
+        mid_band = mid_band/(sfreq)
+        # create second bandpass filter for EMG
+        b1, a1 = sp.signal.butter(4, [mid_band, high_band], btype='bandstop')
+        emg_filtered2 = sp.signal.filtfilt(b1, a1, emg_filtered)
 
-    return emg_filtered2
+        emg[column] = emg_filtered2
+        print(column)
+    print(emg)
+    return emg
+
+def norm_data(df):
+    scaler = preprocessing.MinMaxScaler(feature_range=(-127, 127))
+    df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+    return df
+
 
 def calc_mean_and_variance_column(df):
     data = pd.DataFrame(columns=[
@@ -86,69 +98,98 @@ def calc_mean_and_variance_column(df):
     # print(variances.join(means))
     return(means.join(variances))
 
-def zero_crossings(df):
+def zero_crossings(df, columns):
     zcr = []
-    for i in range(2, 10):
-        column = df.iloc[:, i]
-        zcr.append(np.where(np.diff(np.sign(column))))
-    return zcr
+    for i in range(8):
+        zcr.append(np.where(np.diff(np.sign(df[columns[i]].values)))[0])
+    new_zcr = []
+    for i in range(8):
+        zcr_sensor = []
+        for j in range(len(zcr[i])-1):
+            zcr_sensor.append(zcr[i][j+1] - zcr[i][j])
+        new_zcr.append(np.mean(zcr_sensor))
+    return new_zcr
 
 def signal_change(df):
     sc = []
     for i in range(0, 8):
         signal_changed = []
-        changed_count = 0
-        column = df.loc[:, 'Sensor '+str(i)]
         for j in range(1, 500-1):
-            val1 = np.abs(column[j]) - np.abs(column[j-1])
-            val2 = np.abs(column[j+1]) - np.abs(column[j])
-            #print(column[j-1], column[j], column[j+1], np.sign(val1), np.sign(val2), np.sign(val1)+np.sign(val2))
-            if np.sign(val1) + np.sign(val2) == 0 and (val1 != 0 or val2 != 0):
-                signal_changed.append(j)
-                changed_count += 1
-    return signal_changed
+            column = df.loc[:, 'Sensor '+str(i)]
 
-def wilson_amplitude(df, th):
+            center_value = column[j]
+            lower_value = column[j-1]
+            upper_value = column[j+1]
+
+            sig_center = np.sign(center_value)
+            sig_lower = np.sign(lower_value)
+            sig_upper = np.sign(upper_value)
+
+            #print(sig_lower, sig_center, sig_upper)
+            #print(j, lower_value, center_value, upper_value)
+
+            if sig_center == sig_lower and sig_center == sig_upper:
+                if (np.abs(center_value) > np.abs(lower_value)) and (np.abs(center_value) > np.abs(upper_value)):
+                    signal_changed.append(j)
+            elif sig_upper == sig_lower and sig_lower != sig_center:
+                signal_changed.append(j)
+            elif sig_center == sig_lower and sig_center != sig_upper:
+                if np.abs(center_value) >= np.abs(lower_value):
+                    signal_changed.append(j)
+            elif sig_center != sig_lower and sig_lower == sig_upper:
+                if np.abs(center_value) >= np.abs(upper_value):
+                    signal_changed.append(j)
+        sc.append(signal_changed)
+    new_signal_changed = []
+    for i in range(8):
+        sensor_sc = []
+        for j in range(len(sc[i]) - 1):
+            sensor_sc.append(sc[i][j+1] - sc[i][j])
+        new_signal_changed.append(np.mean(sensor_sc))
+    return new_signal_changed
+
+def wilson_amplitude(emg, th):
     # TODO fazer para todos os dados
     sensor = []
     inds = 0
     ind = 0
-    df2 = df.copy()
+    df2 = emg.copy()
 
-    s0 = np.array(df['Sensor 0'])
-    s1 = np.array(df['Sensor 1'])
-    s2 = np.array(df['Sensor 2'])
-    s3 = np.array(df['Sensor 3'])
-    s4 = np.array(df['Sensor 4'])
-    s5 = np.array(df['Sensor 5'])
-    s6 = np.array(df['Sensor 6'])
-    s7 = np.array(df['Sensor 7'])
+    for column in emg:
+        s0 = np.array(emg['Sensor 0'])
+        s1 = np.array(emg['Sensor 1'])
+        s2 = np.array(emg['Sensor 2'])
+        s3 = np.array(emg['Sensor 3'])
+        s4 = np.array(emg['Sensor 4'])
+        s5 = np.array(emg['Sensor 5'])
+        s6 = np.array(emg['Sensor 6'])
+        s7 = np.array(emg['Sensor 7'])
 
-    ind_th0 = (s0 < th) & (-th < s0)
-    ind_th1 = (s1 < th) & (-th < s1)
-    ind_th2 = (s2 < th) & (-th < s2)
-    ind_th3 = (s3 < th) & (-th < s3)
-    ind_th4 = (s4 < th) & (-th < s4)
-    ind_th5 = (s5 < th) & (-th < s5)
-    ind_th6 = (s6 < th) & (-th < s6)
-    ind_th7 = (s7 < th) & (-th < s7)
-    s0[ind_th0] = 0
-    s1[ind_th1] = 0
-    s2[ind_th2] = 0
-    s3[ind_th3] = 0
-    s4[ind_th4] = 0
-    s5[ind_th5] = 0
-    s6[ind_th6] = 0
-    s7[ind_th7] = 0
+        ind_th0 = (s0 < th) & (-th < s0)
+        ind_th1 = (s1 < th) & (-th < s1)
+        ind_th2 = (s2 < th) & (-th < s2)
+        ind_th3 = (s3 < th) & (-th < s3)
+        ind_th4 = (s4 < th) & (-th < s4)
+        ind_th5 = (s5 < th) & (-th < s5)
+        ind_th6 = (s6 < th) & (-th < s6)
+        ind_th7 = (s7 < th) & (-th < s7)
+        s0[ind_th0] = 0
+        s1[ind_th1] = 0
+        s2[ind_th2] = 0
+        s3[ind_th3] = 0
+        s4[ind_th4] = 0
+        s5[ind_th5] = 0
+        s6[ind_th6] = 0
+        s7[ind_th7] = 0
 
-    df2['Sensor 0'] = s0
-    df2['Sensor 1'] = s1
-    df2['Sensor 2'] = s2
-    df2['Sensor 3'] = s3
-    df2['Sensor 4'] = s4
-    df2['Sensor 5'] = s5
-    df2['Sensor 6'] = s6
-    df2['Sensor 7'] = s7
+        df2['Sensor 0'] = s0
+        df2['Sensor 1'] = s1
+        df2['Sensor 2'] = s2
+        df2['Sensor 3'] = s3
+        df2['Sensor 4'] = s4
+        df2['Sensor 5'] = s5
+        df2['Sensor 6'] = s6
+        df2['Sensor 7'] = s7
 
     return df2
 
@@ -176,11 +217,10 @@ if len(sys.argv) >= 3 :
            'Sensor Variance', 'Gyro Mean', 'Gyro Variance','Orientation Mean',
            'Orientation Variance'])
 
-
     #verifica se arquivo existe. se nçaoe xiste cria um df novo, senao abre o csv
-    feat = Path(current_dir +'/datasets/oficial/features.csv')
+    feat = Path(current_dir +'/datasets/oficial/features2.csv')
     if feat.is_file():
-        features = pd.read_csv(current_dir +'/datasets/oficial/features.csv')
+        features = pd.read_csv(current_dir +'/datasets/oficial/features2.csv')
     else:
         features = pd.DataFrame(columns=[
             'Gyro 0 Variance', 'Gyro 1 Variance', 'Gyro 2 Variance',
@@ -191,7 +231,8 @@ if len(sys.argv) >= 3 :
             'Gyro 0 Mean', 'Gyro 1 Mean', 'Gyro 2 Mean',
             'Orientation 0 Mean', 'Orientation 1 Mean', 'Orientation 2 Mean', 'Orientation 3 Mean',
             'Sensor 0 Mean','Sensor 1 Mean', 'Sensor 2 Mean', 'Sensor 3 Mean', 'Sensor 4 Mean',
-            'Sensor 5 Mean','Sensor 6 Mean', 'Sensor 7 Mean'
+            'Sensor 5 Mean','Sensor 6 Mean', 'Sensor 7 Mean', 'zcr 0', 'zcr 1', 'zcr 2', 'zcr 3', 'zcr 4',
+            'zcr 5', 'zcr 6', 'zcr 7', 'sch 0', 'sch 1', 'sch 2', 'sch 3', 'sch 4', 'sch 5', 'sch 6', 'sch 7'
             ])
 
     # itera sobre todos os csv da pasta indicada
@@ -202,32 +243,41 @@ if len(sys.argv) >= 3 :
         filename = f.replace(pasta,'')
         dados = pd.read_csv(f).drop(columns=['Unnamed: 0','Timestamp','Label'])
 
+        print('Descrição dos dados: ')
+        print(dados.describe())
         #----------- TODO testar isso
         # Filtrar, normalizar, zero crossings, signal change, calcular features e tocar tudo pra um csv
-        filtered = filter_data(dados)
+        emg_columns = ['Sensor 0', 'Sensor 1','Sensor 2', 'Sensor 3', 'Sensor 4', 'Sensor 5', 'Sensor 6', 'Sensor 7']
+        #dados[emg_columns] = filter_data(dados[emg_columns])
+        dados[emg_columns] = wilson_amplitude(dados[emg_columns], 7)
+        print(dados.head())
 
-        #TODO falta normalizar
-        normalized = filtered
+        normalized = norm_data(dados)
+        print(normalized.head(10))
 
         #TODO arrumar zero crossings pro formato que o prof disse na aula?
-        zero_cr = zero_crossings(normalized)
+        zero_cr = zero_crossings(normalized, emg_columns)
         signal_ch = signal_change(normalized)
-        features = features.append(zero_cr, ignore_index=True)
-        features = features.append(signal_ch, ignore_index=True)
+        print('zero: ', zero_cr)
+        print('signal_changed: ', signal_ch)
+        df_zero = pd.DataFrame([zero_cr], columns=['zcr '+str(i) for i in range(8)])
+        df_sch = pd.DataFrame([signal_ch], columns=['sch '+str(i) for i in range(8)])
+        df_zero = df_zero.join(df_sch)
 
         features_temp = calc_mean_and_variance_column(normalized)
         features_temp['Label'] = Label
+        features_temp = features_temp.join(df_zero)
         features = features.append(features_temp, ignore_index=True)
 
         new_name = filename.replace('.csv','novo.csv')
         # print(new_name)
         if not os.path.exists(pasta_treated):
             os.mkdir(pasta_treated)
-        dados_novos.to_csv(pasta_treated +'/'+ new_name,index = False)
-        compilado = compilado.append(dados_novos,ignore_index=True)
+        #dados_novos.to_csv(pasta_treated +'/'+ new_name,index = False)
+        #compilado = compilado.append(dados_novos,ignore_index=True)
 
-    compilado.to_csv(current_dir + '/datasets/oficial/compilado.csv',index = False)
-    features.to_csv(current_dir +'/datasets/oficial/features.csv',index = False)
+    #compilado.to_csv(current_dir + '/datasets/oficial/compilado.csv',index = False)
+    features.to_csv(current_dir +'/datasets/oficial/featuresv2.csv',index = False)
 
 else:
     print("missing argument")
